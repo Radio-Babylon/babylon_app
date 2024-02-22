26e821197547fef324ec52c1e9fcc22239c85fc9
MathiasBabylon <mathiase.babylon@gmail.com>
2024-02-22 16:52:20 +0000

    feat: news page

--- lib/news.dart
+++ lib/news.dart
@@ -1,42 +1,141 @@
+import 'package:babylon_app/models/post.dart';
 import 'package:babylon_app/service/wpGraphQL/wpGraphQLService.dart';
+import 'package:babylon_app/utils/htmlStrip.dart';
+import 'package:babylon_app/utils/launchUrl.dart';
 import 'package:flutter/material.dart';
 import 'reading_news.dart';
 
 class NewsScreen extends StatelessWidget {
+  const NewsScreen({super.key});
+
+    @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: FutureBuilderNews(),
+    );
+  }
+}
+
+class FutureBuilderNews extends StatefulWidget {
+  const FutureBuilderNews({super.key});
+
+  @override
+  State<FutureBuilderNews> createState() => _FutureBuilderNewsState();
+}
+
+class _FutureBuilderNewsState extends State<FutureBuilderNews>{
+  
+  final Future<List<Post>> _posts = WpGraphQLService.getNewPosts();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          const Text(
-            'News Screen',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w300,
-              fontFamily: 'Lato',
-            ),
-          ),
-          const SizedBox(height: 48), // Space between catchphrase and buttons
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 21),
-            child: ElevatedButton(
-              onPressed: () {
-                WpGraphQLService.getNewPosts();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => readingNews()),
-                );
-              },
-              child: const Text('Read the news'),
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(350, 80), // Set the button size
-                textStyle: const TextStyle(fontSize: 24, fontFamily: 'Lato'),
+    return DefaultTextStyle(
+      style: Theme.of(context).textTheme.displayMedium!,
+      textAlign: TextAlign.center,
+      child: FutureBuilder<List<Post>>(
+        future: _posts, // a previously-obtained Future<String> or null
+        builder: (BuildContext context, AsyncSnapshot<List<Post>> snapshot) {
+          List<Widget> children;
+          if (snapshot.hasData) {
+            children = <Widget>[
+              Padding(padding: EdgeInsets.only(left: 16,top: 16),
+                child: Text(
+                "LATEST NEWS",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
               ),
-            ),
-          ),
-        ],
+              ),
+              ...snapshot.data!.map((aPost) => 
+                Card(
+                  margin: EdgeInsets.all(16),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(padding: EdgeInsets.only(left: 8,top: 16),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20), // Image border
+                          child: SizedBox.fromSize(
+                            size: Size.fromRadius(50), // Image radius
+                            child: Image.network(
+                              aPost.featuredImageURL!,
+                              fit: BoxFit.cover),
+                          ),
+                        ),
+                      ),
+                      Expanded(child: Padding(padding: EdgeInsets.all(16),
+                        child: Column(children: [
+                          Text(
+                              aPost.title!,
+                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                          Text(
+                            stripHtml(aPost.excerpt!),
+                            style: TextStyle(fontSize: 12),
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis
+                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            heightFactor: 1.4,
+                            child: TextButton(
+                              onPressed:() => goToUrl("https://babylonradio.com/"+ aPost.url!), child: Text("READ",textAlign: TextAlign.right),
+                              style: TextButton.styleFrom(
+                                foregroundColor: Colors.white,
+                                elevation: 2,
+                                backgroundColor: Color(0xFF006400)
+                              ),
+                            ),
+                          )
+                        ]),
+                      ))
+                    ],
+                  ),
+                ),
+              )
+            ];
+          } else if (snapshot.hasError) {
+            children = <Widget>[
+              const Icon(
+                Icons.error_outline,
+                color: Colors.red,
+                size: 60,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text('Error: ${snapshot.error}'),
+              ),
+            ];
+          } else {
+            children = <Widget>[
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 16),
+                    child: SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: CircularProgressIndicator(color: Color(0xFF006400)),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 16),
+                    child: Text('Loading...'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 128),
+                    child: Image.asset('assets/images/logoRectangle.png',
+                      height: 90,
+                      width: 365
+                    ),
+                  ),
+                ],
+              )
+            ];
+          }
+          return ListView(
+            children: children,
+          );
+        },
       ),
     );
   }
