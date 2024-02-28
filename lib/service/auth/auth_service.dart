44cac5e0560e46c43630438361dec58590d32252
MathiasBabylon <mathiase.babylon@gmail.com>
2024-02-28 10:49:23 +0000

    wip: google providerlogin

--- lib/service/auth/auth_service.dart
+++ lib/service/auth/auth_service.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   static Future<User?> registerUsingEmailPassword({
@@ -25,6 +26,8 @@ class AuthService {
   }
 
   static Future<User?> signInUsingEmailPassword({
+
+    
     required String email,
     required String password,
   }) async {
@@ -41,5 +44,22 @@ class AuthService {
       throw(e);
     }
     return user;
+  }
+
+  static Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
